93fd844b1e85366ee9c1c4a3fb9e9399220534cc
mboben <marko@flare.network>
2024-03-12 14:55:51 +0100

    Prioritised submitter contract (#26)
    * Added prioritised handling of submitter contract
    
    * Fork times for submitter contract
    
    * Simplified function for checking ftso prioritised contract
    
    * Change of default attestors addresses
    
    * Updated submitter fork addresses
    
    * Updated submitter fork test date
    
    * Fixed failing test
    
    * Updated patch version
    
    * Changed submitter fork time for coston 2
    
    ---------
    
    Co-authored-by: Marko Boben <marko.boben@abelium.com>

--- coreth/core/state_transition.go
+++ coreth/core/state_transition.go
@@ -56,8 +56,10 @@ The state transitioning model does all the necessary work to work out a valid ne
 3) Create a new state object if the recipient is \0*32
 4) Value transfer
 == If contract creation ==
-  4a) Attempt to run transaction data
-  4b) If valid, use result as code for the new state object
+
+	4a) Attempt to run transaction data
+	4b) If valid, use result as code for the new state object
+
 == end ==
 5) Run Script section
 6) Derive new state root
@@ -300,13 +302,13 @@ func (st *StateTransition) preCheck() error {
 // TransitionDb will transition the state by applying the current message and
 // returning the evm execution result with following fields.
 //
-// - used gas:
-//      total gas used (including gas being refunded)
-// - returndata:
-//      the returned data from evm
-// - concrete execution error:
-//      various **EVM** error which aborts the execution,
-//      e.g. ErrOutOfGas, ErrExecutionReverted
+//   - used gas:
+//     total gas used (including gas being refunded)
+//   - returndata:
+//     the returned data from evm
+//   - concrete execution error:
+//     various **EVM** error which aborts the execution,
+//     e.g. ErrOutOfGas, ErrExecutionReverted
 //
 // However if any consensus issue encountered, return the error directly with
 // nil evm execution result.
@@ -417,8 +419,8 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	}
 
 	st.refundGas(rules.IsApricotPhase1)
-	if vmerr == nil && msg.To() != nil && *msg.To() == common.HexToAddress(GetPrioritisedFTSOContract(timestamp)) && st.initialGas <= GetMaxFTSOGasLimit(timestamp) {
-		nominalGasUsed := uint64(params.TxGas)                    // 21000
+	if vmerr == nil && IsPrioritisedContractCall(chainID, msg.To(), ret, timestamp) && st.initialGas <= GetMaxFTSOGasLimit(timestamp) {
+		nominalGasUsed := params.TxGas                            // 21000
 		nominalGasPrice := uint64(params.ApricotPhase4MinBaseFee) // 25_000_000_000; the max base fee is 1_000_000_000_000
 		nominalFee := new(big.Int).Mul(new(big.Int).SetUint64(nominalGasUsed), new(big.Int).SetUint64(nominalGasPrice))
 		actualGasUsed := st.gasUsed()
