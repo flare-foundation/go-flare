f3bf0132b00d03a2d43be21a414374ee6ec713b8
Marko Boben <marko.boben@gmail.com>
2024-10-15 19:28:27 -0400

    Do not return error in GetNextStakerChangeTime for Songbird networks

--- avalanchego/vms/platformvm/txs/executor/proposal_tx_executor.go
+++ avalanchego/vms/platformvm/txs/executor/proposal_tx_executor.go
@@ -42,6 +42,10 @@ var (
 	errAdvanceTimeTxIssuedAfterBanff = errors.New("AdvanceTimeTx issued after Banff")
 )
 
+var (
+	songbirdLatestStakingTime = time.Date(2024, time.December, 31, 0, 0, 0, 0, time.UTC)
+)
+
 type ProposalTxExecutor struct {
 	// inputs, to be filled before visitor methods are called
 	*Backend
@@ -572,6 +576,10 @@ func GetNextStakerChangeTime(state state.Chain) (time.Time, error) {
 	case hasPendingStaker:
 		return pendingStakerIterator.Value().NextTime, nil
 	default:
+		// Due to no initial stakers in genesis for Songbird networks
+		if state.GetNetworkID() == constants.SongbirdID || state.GetNetworkID() == constants.CostonID || state.GetNetworkID() == constants.LocalID {
+			return songbirdLatestStakingTime, nil
+		}
 		return time.Time{}, database.ErrNotFound
 	}
 }
