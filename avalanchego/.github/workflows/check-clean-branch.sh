68282927b96b43b27d3cafa0c04508619d9c95cc
Marko Boben <marko.boben@gmail.com>
2025-09-17 16:46:37 +0200

    Remove github workflows file from coreth and avalanche subdirs

--- avalanchego/.github/workflows/check-clean-branch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-git add --all
-git update-index --really-refresh >> /dev/null
-
-# Exits if any uncommitted changes are found.
-git diff-index --quiet HEAD
