0d151e286c65b1571319ebfcc2318c0917315644
Marko Boben <marko.boben@gmail.com>
2025-09-22 16:45:21 +0200

    Add p-chain transfer examples

---
+++ test-scripts/src/test-p-chain-transfer.ts
@@ -0,0 +1,29 @@
+import { pvm, utils, TransferableOutput } from '@flarenetwork/flarejs';
+import { issuePChainTx, localFlareContext } from './utils';
+import { runTest } from './runner';
+
+async function PtoCExport(amountFLR: number, toAddress: string) {
+    const ctx = await localFlareContext();
+
+    // Create and issue a P chain transfer transaction (BaseTx)
+    console.log(`Creating P chain transfer transaction for ${amountFLR} FLR to address ${toAddress}...`);
+
+    const { utxos: utxosp } = await ctx.pvmapi.getUTXOs({
+        addresses: [ctx.addressP]
+    });
+    const exportTx = pvm.newBaseTx(
+        ctx.context,
+        [utils.bech32ToBytes(ctx.addressP)],
+        utxosp,
+        [
+            TransferableOutput.fromNative(ctx.context.avaxAssetID, BigInt(amountFLR * 1e9), [
+                utils.bech32ToBytes(toAddress)
+            ])
+        ]
+    );
+    await issuePChainTx(ctx.pvmapi, exportTx, ctx.privateKey);
+}
+
+
+runTest(() => PtoCExport(100, "P-localflare1zjaa3yjnzn5cjx9r56x59raam2jgwnmztlg995"))
+
