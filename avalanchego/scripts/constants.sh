0739f14c84662f331110710367953a5b05ea966b
Marko Boben <marko.boben@gmail.com>
2025-12-04 18:54:59 +0100

    Fix git path in scripts

--- avalanchego/scripts/constants.sh
+++ avalanchego/scripts/constants.sh
@@ -43,7 +43,7 @@ fi
 
 # Current commit (shared between image build and its test script)
 # WARNING: this will use the most recent commit even if there are un-committed changes present
-full_commit_hash="$(git --git-dir="$AVALANCHE_PATH/.git" rev-parse HEAD)"
+full_commit_hash="$(git --git-dir="$AVALANCHE_PATH/../.git" rev-parse HEAD)"
 commit_hash="${full_commit_hash::8}"
 
 git_commit=${AVALANCHEGO_COMMIT:-$( git rev-list -1 HEAD )}
--- avalanchego/scripts/git_commit.sh
+++ avalanchego/scripts/git_commit.sh
@@ -8,5 +8,5 @@ set -euo pipefail
 AVALANCHE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd ) # Directory above this script
 
 # WARNING: this will use the most recent commit even if there are un-committed changes present
-git_commit="${AVALANCHEGO_COMMIT:-$(git --git-dir="${AVALANCHE_PATH}/.git" rev-parse HEAD)}"
+git_commit="${AVALANCHEGO_COMMIT:-$(git --git-dir="${AVALANCHE_PATH}/../.git" rev-parse HEAD)}"
 commit_hash="${git_commit::8}"
