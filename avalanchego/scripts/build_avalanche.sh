22cc25e46c50b9088b9353bc1ce58b5b028179e7
Gal Lindic <gal.lindic@flare.network>
2026-01-09 09:24:01 +0100

    feat: add SLSA BYOB provenance generation

--- avalanchego/scripts/build_avalanche.sh
+++ avalanchego/scripts/build_avalanche.sh
@@ -29,4 +29,12 @@ source "$AVALANCHE_PATH"/scripts/constants.sh
 
 build_args="$race"
 echo "Building AvalancheGo..."
-go build $build_args -modcacherw -ldflags "-X github.com/ava-labs/avalanchego/version.GitCommit=$git_commit $static_ld_flags" -o "$avalanchego_path" "$AVALANCHE_PATH/main/"*.go
+
+go build \
+  -trimpath \
+  -buildvcs=false \
+  $build_args \
+  -modcacherw \
+  -ldflags "-X github.com/ava-labs/avalanchego/version.GitCommit=$git_commit $static_ld_flags" \
+  -o "$avalanchego_path" \
+  "$AVALANCHE_PATH/main/"*.go
