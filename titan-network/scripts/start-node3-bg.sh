7677d016d07f7e37de2b37c822e79284e71fed21
Erick Pacheco <elina13@wgu.edu>
2026-06-15 22:59:16 -0400

    chore: stabilize local titan network and solidity workspace setup

--- titan-network/scripts/start-node3-bg.sh
+++ titan-network/scripts/start-node3-bg.sh
@@ -5,6 +5,7 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 ROOT_WIN="$(cygpath -w "$ROOT")"
 TITAN_MOUNT="$ROOT_WIN\\titan-network:/app/titan"
 DB_MOUNT="$ROOT_WIN\\db-node3:/app/db"
+STAKING_MOUNT="$ROOT_WIN\\staking-node3:/root/.avalanchego/staking"
 
 bash "$ROOT/titan-network/scripts/create-network.sh"
 docker rm -f titan-node3 >/dev/null 2>&1 || true
@@ -16,6 +17,7 @@ MSYS_NO_PATHCONV=1 MSYS2_ARG_CONV_EXCL='*' docker run -d \
   -p 9655:9651 \
   -v "$TITAN_MOUNT" \
   -v "$DB_MOUNT" \
+  -v "$STAKING_MOUNT" \
   -e AUTOCONFIGURE_PUBLIC_IP=0 \
   -e AUTOCONFIGURE_BOOTSTRAP=1 \
   -e AUTOCONFIGURE_BOOTSTRAP_ENDPOINT=http://titan-node1:9650/ext/info \
